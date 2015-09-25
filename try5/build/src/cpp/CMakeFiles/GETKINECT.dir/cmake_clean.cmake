c9ea851681cfced3a0dc96cee16e1663130f4864
LukeZhu <luke.zhezhu@yahoo.com>
2015-09-25 16:10:50 +0800

    rgbd-slam practice, map build finish

---
+++ try5/build/src/cpp/CMakeFiles/GETKINECT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/GETKINECT.dir/Kinect_Input.cpp.o"
+  "../../../lib/libGETKINECT.pdb"
+  "../../../lib/libGETKINECT.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/GETKINECT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
