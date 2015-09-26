a2d976e6ac02bc589377ea5356920510f151792c
LukeZhu <luke.zhezhu@yahoo.com>
2015-09-26 17:20:29 +0800

    Add try7

---
+++ try7/build/src/cpp/CMakeFiles/POINTCLOUDPROCESS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/POINTCLOUDPROCESS.dir/PointCloudProcess.cpp.o"
+  "../../../lib/libPOINTCLOUDPROCESS.pdb"
+  "../../../lib/libPOINTCLOUDPROCESS.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/POINTCLOUDPROCESS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
